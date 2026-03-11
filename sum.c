9da636a262d915a51d525f3a3f9e0455156e101e
sonu priya <245123751013@mvsrec.edu.in>
2026-03-11 12:02:18 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+ int sum=0;
+int num=100;
+    while (num != 0) {
+        // Extract the last digit using the modulo operator (%)
+        rem = num % 10;
+
+        // Add the extracted digit to the sum
+        sum = sum + rem; // Can also use shorthand: sum += rem;
+
+        // Remove the last digit by dividing the number by 10 (integer division)
+        num = num / 10;
+    }
+
+    // Print the final sum of the digits
+    printf("Sum of digits of the number is %d\n", sum);
+
+    return 0;
+}
